d28a8b129d8eecc9190ccb041620511ee2423567
elfruutas <yastin.villarroel.cancino@alumnos.sip.cl>
2023-10-02 09:22:17 -0300

    Le puse linnnnnnnnnnnnnnnnnnnnnnnnksss

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 var menuDesplegables = document.querySelectorAll(".menu-desplegable");
 
-menuDesplegables.forEach(function(menuDesplegable) {
+menuDesplegables.forEach(function (menuDesplegable) {
     var botonMenu = menuDesplegable.querySelector(".boton-menu");
     var listaCategorias = menuDesplegable.querySelector(".lista-categorias");
 
     // Ocultar la lista al principio
     listaCategorias.style.display = "none";
 
-    botonMenu.addEventListener("mouseenter", function() {
+    botonMenu.addEventListener("mouseenter", function () {
         listaCategorias.style.display = "block";
     });
 
-    botonMenu.addEventListener("mouseleave", function() {
+    botonMenu.addEventListener("mouseleave", function () {
         listaCategorias.style.display = "none";
     });
 
-    listaCategorias.addEventListener("mouseenter", function() {
+    listaCategorias.addEventListener("mouseenter", function () {
         listaCategorias.style.display = "block";
     });
 
-    listaCategorias.addEventListener("mouseleave", function() {
+    listaCategorias.addEventListener("mouseleave", function () {
         listaCategorias.style.display = "none";
     });
-});                                                 
+});
+
+function resaltarElemento(elemento) {
+    elemento.classList.add("resaltado"); 
+}
+function restaurarElemento(elemento) {
+    elemento.classList.remove("resaltado"); 
+}
