922c6534dc50abe5cb164820d10b7f00e06cf89b
fpxx777 <renato.fox.castillo@alumnos.sip.cl>
2023-10-13 12:49:50 -0300

    probando

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
-var click = false;
+var click = true;
 var cat = document.getElementById("cat")
 
-function ClicleameEsta(){
-    if(click == false){
-        cat.innerHTML = '<h3 onclick="ClicleameEsta()">Categorias ⮞</h3> <h3>Farmacia ⮞</h3> <h3>Espacial x ⮞</h3> <h3>Perros ⮞</h3> <h3>Gatos ⮞</h3> <h3>Exoticos ⮞</h3> <h3>Marcas ⮞</h3>';
+
+cat.style.display = "none"
+const productoo = [
+    {
+        marca: 'Pedigree',
+        nombre: 'Alimento Perro Adulto Pedigree Carne Pollo y Cereal 3 kg',
+        precio: '$10.990',
+        descuento: '50%',
+        precioAnterior: '%20.990'
+    },
+    {
+        marca: 'Champion Dog',
+        nombre: 'Alimento Seco Para Perro Adulto 15 kg Carne y Cereal',
+        precio: '$15.990',
+        descuento: '25%',
+        precioAnterior: '$20.990'
+    },
+    {
+        marca: 'Champion Dog',
+        nombre: 'Alimento de Perro Senior 2,7 kg',
+        precio: '$12.500',
+        descuento: '50%',
+        precioAnterior: '$25.000'
+    },
+]
+
+
+var img = document.getElementsByClassName("imag");
+var marc = document.getElementsByClassName("marca");
+var productName = document.getElementsByClassName("product-name");
+var precioo = document.getElementsByClassName("precio")
+var precioPasado = document.getElementsByClassName("precio-tachado")
+
+
+for (var i = 0; i < 4; i++) {
+document.getElementById("[i]").innerHTML = `
+<div class="product-img"> <img class="imag" src="img/Perro.jpg" alt=""> </div>`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function ClicleameEsta() {
+    if (click == false) {
+        cat.style.display = "none";
         click = true;
     }
-    else if(click == true){
-        cat.innerHTML = '<h3 onclick="ClicleameEsta()">Categorias ⮞</h3>';
+    else if (click == true) {
+        cat.style.display = "flex";
         click = false;
     }
 }
